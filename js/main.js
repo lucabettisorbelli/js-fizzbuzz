e28487bfc15755ec040cfde86f9d6037a97b0a9e
luca betti sorbelli <luca betti sorbelli>
2023-04-17 17:29:27 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 
-const contenitore = document.getElementById("container");
+let container = document.getElementById("container");
 
 for (let i = 1; i <= 100; i++) {
-
-    // variabili specifiche per valori divisibili
-    let valoreDiviso3 = ""; 
-    let valoreDiviso5 = ""; 
-
-    // se il resto di "i" diviso 3 = 0
+    let valore = "";
+    let className = "";
     if (i % 3 === 0) {
-        valoreDiviso3 += "Fizz";
+        valore += "Fizz";
+        className = "box1";
     }
-
-    // se il resto di "i" diviso 5 = 0
     if (i % 5 === 0) {
-        valoreDiviso5 += "Buzz";
+        valore += "Buzz";
+        className = "box2";
     }
-
-    // stampa valore(se non è stringa vuota) o "i"
-    console.log((valoreDiviso3 + valoreDiviso5) || i); 
-
-    // contenitore.innerHTML += `<div class="box">${valore}</div>`
+    if (i % 3 === 0 && i % 5 === 0) {
+        className = "box3";
+    }
+    if (valore === "") {
+        valore = i;
+        className = "box4";
+    }
+    console.log(valore);
+    let elemento = document.createElement("div");
+    elemento.textContent = valore;
+    elemento.classList.add(className);
+    container.append(elemento);
 }
