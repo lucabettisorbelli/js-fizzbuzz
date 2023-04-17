e917c6193326f387cdeb3338c138f629b1c5edf5
luca betti sorbelli <luca betti sorbelli>
2023-04-17 15:55:05 +0200

    stsampa in console

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+
+const contenitore = document.getElementById("container");
+
 for (let i = 1; i <= 100; i++) {
-    console.log (i)
+
+    // variabili specifiche per valori divisibili
+    let valoreDiviso3 = ""; 
+    let valoreDiviso5 = ""; 
+
+    // se il resto di "i" diviso 3 = 0
+    if (i % 3 === 0) {
+        valoreDiviso3 += "Fizz";
+    }
+
+    // se il resto di "i" diviso 5 = 0
+    if (i % 5 === 0) {
+        valoreDiviso5 += "Buzz";
+    }
+
+    // stampa valore(se non è stringa vuota) o "i"
+    console.log((valoreDiviso3 + valoreDiviso5) || i); 
+
+    // contenitore.innerHTML += `<div class="box">${valore}</div>`
 }
