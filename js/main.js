640b0245416c58488ed57f65bc1eabc19bfb34c6
luca betti sorbelli <luca betti sorbelli>
2023-04-18 10:32:12 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 
 let container = document.getElementById("container");
 
+
 for (let i = 1; i <= 100; i++) {
     let valore = "";
     let className = "";
-    if (i % 3 === 0) {
-        valore += "Fizz";
-        className = "box1";
-    }
-    if (i % 5 === 0) {
-        valore += "Buzz";
-        className = "box2";
-    }
+
     if (i % 3 === 0 && i % 5 === 0) {
-        className = "box3";
+        valore += "FizzBuzz"
+        className = "box1";
+    } else if (i % 3 === 0) {
+        valore += "Fizz"
+        className = "box2"
+    } else if (i % 5) {
+        valore += "Buzz"
+        className = "box3"
+    } else {
+        valore += i
+        className = "box4"
     }
-    if (valore === "") {
-        valore = i;
-        className = "box4";
-    }
+    
     console.log(valore);
     let elemento = document.createElement("div");
     elemento.textContent = valore;
