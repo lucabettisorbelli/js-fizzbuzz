940374e3563ec471a609b2d00b816a6fd4929466
luca betti sorbelli <luca betti sorbelli>
2023-04-18 10:41:53 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -7,17 +7,19 @@ for (let i = 1; i <= 100; i++) {
     let className = "";
 
     if (i % 3 === 0 && i % 5 === 0) {
-        valore += "FizzBuzz"
+        valore += "FizzBuzz";
         className = "box1";
+
     } else if (i % 3 === 0) {
-        valore += "Fizz"
-        className = "box2"
-    } else if (i % 5) {
-        valore += "Buzz"
-        className = "box3"
+        valore += "Fizz";
+        className = "box2";
+
+    } else if (i % 5 === 0) {
+        valore += "Buzz";
+        className = "box3";
     } else {
-        valore += i
-        className = "box4"
+        valore += i;
+        className = "box4";
     }
     
     console.log(valore);
